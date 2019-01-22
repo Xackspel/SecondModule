26596273ef02a7d9982f11dae6e42838902e19fd
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-01-22 11:13:22 +0200

    removed extra code

--- create.php
+++ create.php
@@ -1,7 +1,5 @@
 <?php
     var_dump($_FILES);
-    if($_FILES['file']['size'] == 6459756){
-        echo "File size is 6459756!";
-    }
+    
     move_uploaded_file($_FILES['file']['tmp_name'], '1.jpg');
 ?>
