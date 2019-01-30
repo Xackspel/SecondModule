ca8c3986cf62fd25ba782f6e15a1625dc68ed4d9
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-01-30 15:50:44 +0200

    Learn some subjects
    Learned some new subjects in PHP

--- testing.php
+++ testing.php
@@ -1,7 +1,9 @@
 <?php
     
-    $second_var = "apple";
-    $first_var = "second_var";
-    echo ${$first_var};
-    var_dump($first_var);
-?>
+    // $second_var = "apple";
+    // $first_var = "second_var";
+    // echo ${$first_var};
+    // var_dump($first_var);
+    $array = array("html", "css", "javascript", "php");
+    var_dump($array);
+    ?>
